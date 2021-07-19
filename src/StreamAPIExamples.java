47b05300f114b4c9fb40481353baed6e3f678867
Mesut Uluag <punk7119@gmail.com>
2021-07-19 09:42:01 +0300

    Stream API Example - convert list of object into comma seperated format - only increment id's of the stream -

--- src/StreamAPIExamples.java
+++ src/StreamAPIExamples.java
@@ -13,6 +13,14 @@ public class StreamAPIExamples {
                                             new Employee(103, "kim", "A", 90000),
                                             new Employee(104, "json", "C", 15000)).collect(Collectors.toList());
         OptionalDouble averageSalary = employees.stream().filter(e-> e.getGrade().equalsIgnoreCase("A")).map(Employee::getSalary).mapToDouble(x->x).average();
-        System.out.println(averageSalary);
+//        System.out.println(averageSalary);
+
+        // only increment id's of Employee using stream
+        employees.stream().forEach(e-> e.setId(e.getId() + 1));
+//        System.out.println(employees.get(0).getId() + "name " + employees.get(0).getName());
+
+       String commaSeperatedNames = employees.stream().map(e-> e.getId() + "," + e.getName()).collect(Collectors.joining(";"));
+//       System.out.println(commaSeperatedNames);
+
     }
 }
